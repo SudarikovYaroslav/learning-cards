ab3a2565bb3f0b5b2ec57958ae9f8cb293a9d01c
Sudarikov_Iaroslav <yarik846@mail.ru>
2021-11-23 16:09:30 +0300

    GUI update

--- src/basicClasses/CardBuilder.java
+++ src/basicClasses/CardBuilder.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class CardBuilder {
@@ -13,6 +16,7 @@ public class CardBuilder {
     private JTextArea backSide;
     private ArrayList<Card> cardList;
     private JFrame frame;
+
 
     public void go(){
         // формируем и выводим на экран GUI включая создание и регистрацию
@@ -27,7 +31,6 @@ public class CardBuilder {
         frontSide.setLineWrap(true);
         frontSide.setWrapStyleWord(true);
         frontSide.setFont(bigFont);
-
         JScrollPane frontScroller = new JScrollPane(frontSide);
         frontScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         frontScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -37,7 +40,6 @@ public class CardBuilder {
         backSide.setLineWrap(true);
         backSide.setWrapStyleWord(true);
         backSide.setFont(bigFont);
-
         JScrollPane backScroller = new JScrollPane(backSide);
         backScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         backScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -59,28 +61,19 @@ public class CardBuilder {
 
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
-
         JMenuItem newMenuItem = new JMenuItem("New");
         JMenuItem saveMenuItem = new JMenuItem("Save");
         newMenuItem.addActionListener(new NewMenuListener());
         saveMenuItem.addActionListener(new SaveMenuListener());
-
         fileMenu.add(newMenuItem);
         fileMenu.add(saveMenuItem);
-        mainPanel.add(fileMenu);
+        menuBar.add(fileMenu);
+        mainPanel.add(menuBar);
 
         frame.setJMenuBar(menuBar);
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         frame.setSize(500,600);
         frame.setVisible(true);
-    }
-
-
-    private void saveFile(File file){
-        // Вызывается классом SaveMenuListener. Непосредственно записывает данные в файл
-        // Проходим по списку карточек и записываем каждый элемент в текстовый файл,
-        // который потом можно будет прчитать
-        //(т.е. с чистыми разделителями между частями)
     }
 
 
@@ -101,21 +94,52 @@ public class CardBuilder {
         // в текущем списке в виде набора
         @Override
         public void actionPerformed(ActionEvent e) {
+            Card card = new Card(frontSide.getText(), backSide.getText());
+            cardList.add(card);
 
+            JFileChooser fileSave = new JFileChooser();
+            fileSave.showSaveDialog(frame);
+            saveFile(fileSave.getSelectedFile());
         }
     }
+
 
     private class NewMenuListener implements ActionListener{
         // Запускается при выборе команды New из меню File означает, что пользователь хочет создать новый набор
         @Override
         public void actionPerformed(ActionEvent e) {
             //Очищаем список карточек и текстовые области
+            cardList.clear();
+            clearCard();
         }
     }
+
 
     private void clearCard(){
         frontSide.setText("");
         backSide.setText("");
         frontSide.requestFocus();
+    }
+
+
+    private void saveFile(File file){
+        // Вызывается классом SaveMenuListener. Непосредственно записывает данные в файл
+        // Проходим по списку карточек и записываем каждый элемент в текстовый файл,
+        // который потом можно будет прчитать
+        //(т.е. с чистыми разделителями между частями)
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            for (Card card : cardList){
+                System.out.println("Test string: " + card.getFront() + " : " + card.getBack());
+                writer.write(card.getFront() + "/");
+                writer.write(card.getBack() + "\n");
+            }
+            writer.close();
+            System.out.println("*********** cards have been saved ***********");
+        } catch (IOException e){
+            System.out.println("Couldn't wright the cardList out");
+            e.printStackTrace();
+        }
     }
 }
