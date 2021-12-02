25243658d4e6e8f963dbb1faa6528d7d41dba1a1
Sudarikov_Iaroslav <yarik846@mail.ru>
2021-12-02 13:16:14 +0300

    Card Builder GUI added

--- src/view/View.java
+++ src/view/View.java
@@ -3,6 +3,7 @@ package view;
 import basicClasses.Dictionary;
 import basicClasses.User;
 import basicClasses.dictionaries.EnglishDictionary;
+import controller.Controller;
 import model.Model;
 import view.GUI.*;
 
@@ -10,15 +11,27 @@ import view.GUI.*;
 public class View {
 
     private Model model;
+    private Controller controller;
+
     private User testUser = new User("Yar"); // line should be deleted after test
     private Dictionary currentDictionary = new EnglishDictionary("English Cards"); // line only for tests. Should be deleted
+
 
     public void setModel(Model model) {
         this.model = model;
     }
 
+
+    public void setController(Controller controller){
+        this.controller = controller;
+    }
+
+
     public void go(){
-       CardBuilderGUI cbGUI = new CardBuilderGUI();
-       cbGUI.go();
+       /*UserSelectGUI usGUI = new UserSelectGUI(model);
+       usGUI.go();*/
+
+        UserBuilderGUI userBuilderGUI = new UserBuilderGUI();
+        userBuilderGUI.go();
     }
 }
